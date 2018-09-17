8706492900c9749ff43caca87871938d3641529d
Jose Delgado <josedelgado9@ufl.edu>
2018-09-17 09:11:39 -0400

    Updated files to complete Assignment 3

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -7,15 +7,37 @@ var fs = require('fs'),
     mongoose = require('mongoose'), 
     Schema = mongoose.Schema, 
     Listing = require('./ListingSchema.js'), 
-    config = require('./config');
+    config = require('./config'),
+    jsonData = require('./listings.json');
 
 /* Connect to your database */
-
+mongoose.connect(config.db.uri);
+// Get notified if successfully connected to database or error occured
+// var dbc = mongoose.connection;
+// dbc.on('error', console.error.bind(console, 'connection error:'));
+// dbc.once('open', function(){
+//   console.log("Database successfully connected");
+// });
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
  */
 
+fs.readFile('listings.json', 'utf8', function(err, listingsInfo){
+  if(err) throw err;
+
+  //convert JSON data into angular format and save in variable
+  var info = JSON.parse(listingsInfo);
+
+  //create objects using information from JSON file and using schema created
+  info.entries.forEach(function(info){
+    var newListing = new Listing(info);
+    newListing.save(function(err){
+      if(err) throw err;
+      console.log('Succesfully saved');
+    });
+  });
+});
 
 /* 
   Once you've written + run the script, check out your MongoLab database to ensure that 
--- queries.js
+++ queries.js
@@ -1,28 +1,61 @@
 /* Fill out these functions using Mongoose queries*/
+var fs = require('fs'),
+    mongoose = require('mongoose'), 
+    Schema = mongoose.Schema,
+    Listing = require('./ListingSchema.js'), 
+    config = require('./config');
+
+//stablish connection to database
+mongoose.connect(config.db.uri);
 
 var findLibraryWest = function() {
   /* 
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
+  Listing.findOne({name: 'Library West'}, function (err, listingsInfo ) {
+    if (err) throw err;
+    console.log('Object Found:\n')
+    console.log(listingsInfo);
+  });
+
 };
+
 var removeCable = function() {
   /*
     Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+  Listing.findOneAndRemove({code: 'CABL'}, function (err) {
+    if (err) throw err;
+    console.log('Removed successfully\n');
+  });
+
 };
+
 var updatePhelpsLab = function() {
   /*
     Phelps Laboratory's address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
    */
+  Listing.findOneAndUpdate({name: 'Phelps Laboratory'}, {address: '1953 Museum Rd, Gainesville, FL 32603'}, function (err, listingsInfo ) {
+    if (err) throw err;
+    console.log('Successfully updated\n');
+    console.log(listingsInfo);
+  });
+
 };
+
 var retrieveAllListings = function() {
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
+  Listing.find({}, function (err, listingsInfo ) {
+    if (err) throw err;
+    console.log(listingsInfo);
+  });
+
 };
 
 findLibraryWest();
